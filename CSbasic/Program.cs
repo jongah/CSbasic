0c4abd26973701ba179e0dd40bcfdf066d100278
jongah <jongah2021@gmail.com>
2021-04-16 15:27:55 +0900

    초기 커밋.

---
+++ CSbasic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSbasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
