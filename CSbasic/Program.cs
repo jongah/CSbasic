5f05ba787075e6b9eba11e6b51509966c232e93a
jongah <jongah2021@gmail.com>
2021-04-23 15:59:40 +0900

    #10 var 키워드 반드시 초기화를 시켜줘야 한다.

--- CSbasic/Program.cs
+++ CSbasic/Program.cs
@@ -127,6 +127,13 @@ namespace CSbasic
             Console.WriteLine(_char.GetType());
             Console.WriteLine(_string.GetType());
 
+            //var키워드 * 반드시 초기화를 시켜줘야한다. ( 초기화 하는 값을 보고 추측하여 할당한다. )
+            //메서드 안에서만 선언할 수 있다. -> 지역변수만 사용가능
+            //멤버변수는 동적으로 불러온다.
+
+            var number = 100;
+            var somea = "인간";
+            var someb = 3.4234242;
         }
     }
 }
