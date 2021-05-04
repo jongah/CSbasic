c56e860e496b52840485dd85d96a7769f5b0d6ff
jongah <jongah2021@gmail.com>
2021-05-04 12:06:59 +0900

    #13 삼항연산자

--- CSBasic3202/Program.cs
+++ CSBasic3202/Program.cs
@@ -76,6 +76,10 @@ namespace CSBasic3202
                     break;
             }
 
+            string input2 = Console.ReadLine();
+            int number = int.Parse(input2);
+            Console.WriteLine((number > 0) ? "자연수" : "자연수 아님");
+
         }
     }
 }
