682dd492230b43e91b2f1340f01b1abfb4680d1e
jongah <jongah2021@gmail.com>
2021-05-04 11:58:44 +0900

    #12 switch 문법

--- CSBasic3202/Program.cs
+++ CSBasic3202/Program.cs
@@ -52,6 +52,30 @@ namespace CSBasic3202
             {
                 Console.WriteLine("...");
             }
+
+
+            Console.WriteLine("몇월 인가요 : ");
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 12: case 1: case 2:
+                    Console.WriteLine("겨울");
+                    break;
+                case 3: case 4: case 5:
+                    Console.WriteLine("봄");
+                    break;
+                case 6: case 7: case 8:
+                    Console.WriteLine("여름");
+                    break;
+                case 9: case 10: case 11:
+                    Console.WriteLine("가을");
+                    break;
+                default:
+                    Console.WriteLine("똑바로 입력해 주세요.");
+                    break;
+            }
+
         }
     }
 }
