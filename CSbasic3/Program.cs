a0ca2f3969f044012259e1f791609dc367b61c7c
jongah <jongah2021@gmail.com>
2021-05-06 09:19:46 +0900

    초기 커밋.

---
+++ CSbasic3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSbasic3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
