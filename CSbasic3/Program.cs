76560102253cc144cc0c917e63fed90906070e80
jongah <jongah2021@gmail.com>
2021-05-06 10:07:56 +0900

    #19 while dowhile

--- CSbasic3/Program.cs
+++ CSbasic3/Program.cs
@@ -13,6 +13,20 @@ namespace CSbasic3
             Console.WriteLine(intArry[99]);
             Console.WriteLine(intArry[intArry.Length]);
 
+            int i = 0; 
+            int[] intArray2 = { 44, 55, 66, 77, 88, 99 };
+            while (i < intArray2.Length)
+            {
+                Console.WriteLine(intArray2[i]);
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.WriteLine("입력 종료");
+                input = Console.ReadLine();
+            } while (input != "exit");
         }
     }
 }
