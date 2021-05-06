8f5714e207adcbe56e1cd84d44a868c4f103eb9e
jongah <jongah2021@gmail.com>
2021-05-06 09:56:23 +0900

    #17 배열의 개념

--- CSbasic3/Program.cs
+++ CSbasic3/Program.cs
@@ -7,6 +7,12 @@ namespace CSbasic3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] intArry = new int[100];
+            Console.WriteLine(intArry.Length);
+            Console.WriteLine(intArry[99]);
+            Console.WriteLine(intArry[intArry.Length]);
+
         }
     }
 }
