d2ef13b495364585985a182ac83b3f4eea8529dc
jongah <jongah2021@gmail.com>
2021-05-27 10:20:47 +0900

    #24

--- CSBasic_4/Class1.cs
+++ CSBasic_4/Class1.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace CSBasic_4
 {
+    class MyMath
+    {
+        int somenumber = 3;
+        public static double PI = 3.444444;
+        public static void Greeting()
+        {
+            Console.WriteLine("greeting");
+            //somenumber += 3;
+        }
+    }
     class Programe
     {
-
+        Product product1 = new Product();
+        Product productA = new Product() { name = "짬뽕", price = 5000 };
     }
 }
