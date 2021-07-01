37e0b9624938344c5d28cb7af477d2103773f9e3
jongah <jongah2021@gmail.com>
2021-07-01 09:22:46 +0900

    #29 클래스 메서드

--- csbasic6/Program.cs
+++ csbasic6/Program.cs
@@ -8,13 +8,46 @@ namespace csbasic6
 {
     class Program
     {
+        public static int instanceMethod()
+        {
+            return 0;
+        }
         static void Main(string[] args)
         {
             MethodExample m = new MethodExample();
             Console.WriteLine(m.Power(4));
+
+            Test test = new Test();
+            Console.WriteLine(test.Sum(1, 100));
+
+            MyMath math = new MyMath();
+
+
         }
-        
+
+        class MyMath
+        {
+            public static int Abs(int input)
+            {
+                return (input < 0) ? -input : input;
+            }
+        }
+
     }
+
+    class Test
+    {
+        public int Sum(int min, int max)
+        {
+            int output = 0; 
+            for(int i = 0; i < max; i++)
+            {
+                output += i;
+            }
+            return output;
+        }
+    }
+
     class MethodExample 
     {
         public int Power(int x)
